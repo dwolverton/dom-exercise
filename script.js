31fd0538eb8885c136914bb453ccd96f5f606e62
David Wolverton <dwolvert@gmail.com>
2021-08-13 09:53:32 -0400

    DOM demo

--- script.js
+++ script.js
@@ -4,7 +4,19 @@
   document.getElementById("actionButton").addEventListener("click", main);
 
   function demo() {
-    
+    const shrinkMeP = document.getElementById("shrink-me");
+    // shrinkMeP.innerText = "Hello <em>Class</em>!";
+    shrinkMeP.innerHTML = "Hello <em>Class</em>!";
+    // adding & removing classes.
+    const allListItems = document.querySelectorAll("li"); // find all <li>s on the page
+    const objectLi = allListItems[1];
+    objectLi.classList.toggle("big");
+    // setting styles
+    const formInput = document.getElementById("name");
+    formInput.style.border = "3px solid magenta";
+    formInput.style.backgroundColor = "LightPink";
+    // set an attribute
+    formInput.setAttribute("placeholder", "Your name here...");
   }
 
   function main() {
